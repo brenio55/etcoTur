ca5eeb4791cac1604b6c9b27bdf08493b921771d
brenio55 <brenio55@outlook.com>
2024-01-31 23:22:53 -0300

    criação do header concluída

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+function Header(){
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    <img src="img/logo.png" alt="logo" />
+                </div>
+                <div className="menu">
+                    <ul>
+                        <li>INÍCIO</li>
+                        <li>SERVIÇOS</li>
+                        <li>CONTATO</li>
+                        <li>SOBRE NÓS</li>
+                        <li>TRABALHE CONOSCO</li>
+                    </ul>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
