dbed6a1a18ffa05f94193c7033dc22e28b70ae01
brenio55 <brenio55@outlook.com>
2024-02-11 17:46:55 -0300

    style: finalizada página contato. Iniciando página Sobre Nós

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,28 +5,31 @@ function Contact () {
         <>
             <section className="contact">
                 <h2>Contato</h2>
-                <div className="flex spaceAround">
+                <div className="flex spaceAround central">
                     <div className="contactL">
                         <h3>Entre em contato conosco por meio do formulário de contato ao lado, ou por meio de alguma das nossas redes sociais!</h3>    
-                        <div className="flex">
+                        <div className="flex redesSociais">
                             <div className="redeSocialContact">
-                                <img src="" alt="" />
+                                <img src="/img/contact/whatsapp.png" alt="whatsapp icon" />
                             </div>
                             <div className="redeSocialContact">
-                                <img src="" alt="" />
+                                <img src="/img/contact/instagram.png" alt="instagram icon" />
                             </div>
                             <div className="redeSocialContact">
-                                <img src="" alt="" />
+                                <img src="/img/contact/linkedin.png" alt="linkedin icon" />
                             </div>
                         </div>
                     </div>
-                    <div className="contactL">
+                    <div className="contactR">
                         <form action="">
                             <input type="text" name="nome" id="" placeholder="Nome"/>
-                            <input type="text" name="mensagem" id="" placeholder="Mensagem"/>
-                            <input type="text" name="respostaSoma" id="" placeholder="4 + 7 = "/>
-
-                            <input type="submit" value="Enviar" className="buttonWhiteBackground" />
+                            
+                            <textarea type="text" name="mensagem" id="" placeholder="Mensagem" cols={40} rows={6}/>
+                        
+                            <div className="flex">
+                                <input type="text" className="respostaSoma" name="respostaSoma" id="" placeholder="4 + 7 = "/>
+                                <input type="submit" value="Enviar" className="buttonWhiteBackground enviarContactButton" />    
+                            </div>
                         </form>
                     </div>
                 </div>
