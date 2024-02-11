4aa5bd0d4fbeb153e38232c7b519e84e60c6b8da
brenio55 <brenio55@outlook.com>
2024-02-11 18:47:33 -0300

    build: mudado informações no cpnel.yml, corrigido bug de imagens na dist!

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,13 +5,13 @@ function Services () {
     return (
         <>
         <section className="services">
-            <img className="bordaSupServices" src="../../public/img/services/bordaSupServiços.png" />
+            <img className="bordaSupServices" src="/img/services/bordaSupServiços.png" />
             <h2>Serviços</h2>
             <div className="flex center servicesDiv">
                 <Service
                     title="Transporte de Passageiros"
                     description="Translados Aeroporto, Faculdades, Transporte Escolar, Transporte de Funcionários, dentre outros."
-                    imgSrc="img/services/transporteDePassageiros.png"
+                    imgSrc="/img/services/transporteDePassageiros.png"
                     buttonType="whiteBackground"
                     buttonValue="Saiba Mais"                
                 ></Service>
@@ -20,7 +20,7 @@ function Services () {
                     description="Passeio mais longo ou viagem
                     recreativa, muitas vezes em grupo e
                     não raro orientado por um guia."
-                    imgSrc="img/services/excursoes.png"
+                    imgSrc="/img/services/excursoes.png"
                     buttonType="whiteBackground"
                     buttonValue="Saiba Mais"                
                 ></Service>
@@ -29,7 +29,7 @@ function Services () {
                     description="transporte de frete, e serviços
                     similares também são prestados
                     conosco!"
-                    imgSrc="img/services/fretamentos.png"
+                    imgSrc="/img/services/fretamentos.png"
                     buttonType="whiteBackground"
                     buttonValue="Saiba Mais"                
                 ></Service>
@@ -38,13 +38,13 @@ function Services () {
                     description="Fretamentos diversos, turismo em
                     geral, guia turístico, dentre outros
                     serviços similares!"
-                    imgSrc="img/services/outrosServicos.png"
+                    imgSrc="/img/services/outrosServicos.png"
                     buttonType="whiteBackground"
                     buttonValue="Saiba Mais"                
                 ></Service>
             </div>
 
-            <img src="../../public/img/services/montanhasServiços.png" alt="" srcset="" className="montanhaServices"/>
+            <img src="/img/services/montanhasServiços.png" alt="" srcset="" className="montanhaServices"/>
         </section>            
         </>
     )
