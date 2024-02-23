5f2b3c01af92dcbdda17b243eb3ee312705a41c8
brenio55 <brenio55@outlook.com>
2024-02-23 00:53:30 -0300

    feat: adicionado responsividade do nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,39 @@
-import UlMenu from "./UlMenu"
+import React, { useState } from "react";
+import UlMenu from "./UlMenu";
 
-function Header(){
+function Header() {
+    const [mostrarMenu, setMostrarMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setMostrarMenu(!mostrarMenu);
+    };
+
     return (
         <>
             <header>
                 <div className="logo">
                     <img src="img/logo.png" alt="logo" />
                 </div>
-                <div className="menu">
+                <svg
+                    className="menuIcon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                    onClick={toggleMenu}
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+                    />
+                </svg>
+                <nav className={`menu ${mostrarMenu ? 'mostrar' : ''}`}>
                     <UlMenu></UlMenu>
-                </div>
+                </nav>
             </header>
         </>
-    )
+    );
 }
 
-export default Header
+export default Header;
